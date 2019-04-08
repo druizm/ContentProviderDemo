01dc32e3081f0cd982a3d5836f98b88d5537469b
druizm <d.ruiz@outlook.com>
2019-04-08 15:09:15 -0500

    feat: read data of user.

--- app/src/main/java/biz/belcorp/dev/contentproviderdemo/MainActivity.kt
+++ app/src/main/java/biz/belcorp/dev/contentproviderdemo/MainActivity.kt
@@ -6,8 +6,6 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import android.content.ContentProviderClient
-import android.content.Intent
 import android.content.pm.PackageManager
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
@@ -15,8 +13,12 @@ import android.support.v4.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
-    var PERMISSIONS = arrayOf(
+    private var PERMISSIONS_ESIKA = arrayOf(
         "biz.belcorp.consultoras.esika.dev.provider.data.READ_DATABASE"
+    )
+
+    private var PERMISSIONS_LBEL = arrayOf(
+        "biz.belcorp.consultoras.lbel.dev.provider.data.READ_DATABASE"
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,41 +27,48 @@ class MainActivity : AppCompatActivity() {
 
 
         btnUserEsika.setOnClickListener {
-            val contentUri = Uri.parse("content://biz.belcorp.maquillador.provider.data/Pedidos")
-            // EXPLICIT INTENT EXAMPLE
-            grantUriPermission("biz.belcorp.maquillador", contentUri, Intent.FLAG_GRANT_READ_URI_PERMISSION)
-
-
-            contentUser(contentUri)
-        }
-
-        btnClientsEsika.setOnClickListener {
-            val contentUri = Uri.parse("content://biz.belcorp.consultoras.esika.provider.data/User")
-            contentUser(contentUri)
-        }
-
-        btnUserLBel.setOnClickListener {
-
             if (ContextCompat.checkSelfPermission(this,
                     "biz.belcorp.consultoras.esika.dev.provider.data.READ_DATABASE") !=
                 PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, PERMISSIONS, 1)
+                ActivityCompat.requestPermissions(this, PERMISSIONS_ESIKA, 1)
             } else {
                 val contentUri = Uri.parse("content://biz.belcorp.consultoras.esika.dev.provider.data/User")
                 contentUser(contentUri)
             }
+        }
 
+        btnClientsEsika.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this,
+                    "biz.belcorp.consultoras.esika.dev.provider.data.READ_DATABASE") !=
+                PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, PERMISSIONS_ESIKA, 1)
+            } else {
+                val contentUri = Uri.parse("content://biz.belcorp.consultoras.esika.dev.provider.data/Client")
+                contentClients(contentUri)
+            }
+        }
+
+        btnUserLBel.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this,
+                    "biz.belcorp.consultoras.lbel.dev.provider.data.READ_DATABASE") !=
+                PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, PERMISSIONS_LBEL, 1)
+            } else {
+                val contentUri = Uri.parse("content://biz.belcorp.consultoras.lbel.dev.provider.data/User")
+                contentClients(contentUri)
+            }
         }
 
         btnClientsLBel.setOnClickListener {
-            val contentUri = Uri.parse("content://biz.belcorp.consultoras.esika.dev.provider.data/Client")
-            contentClients(contentUri)
+            if (ContextCompat.checkSelfPermission(this,
+                    "biz.belcorp.consultoras.esika.dev.provider.data.READ_DATABASE") !=
+                PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, PERMISSIONS_LBEL, 1)
+            } else {
+                val contentUri = Uri.parse("content://biz.belcorp.consultoras.lbel.dev.provider.data/Client")
+                contentUser(contentUri)
+            }
         }
-
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     private fun showToastMessage (msg : String) {
@@ -83,22 +92,21 @@ class MainActivity : AppCompatActivity() {
                 // Loop in the cursor to get each row.
                 do {
 
-
                     // Get column 4 value.
-                    val column4Index = cursor.getColumnIndexOrThrow("Descripcion")
+                    val column4Index = cursor.getColumnIndexOrThrow("ConsultantCode")
                     val column4Value = cursor.getString(column4Index)
 
                     // Get column 5 value.
-                    val column5Index = cursor.getColumnIndex("MarcaDescripcion")
+                    val column5Index = cursor.getColumnIndex("ConsultantName")
                     val column5Value = cursor.getString(column5Index)
 
 
                     // Get column 3 value.
-                    val column3Index = cursor.getColumnIndex("Cantidad")
+                    val column3Index = cursor.getColumnIndex("Campaing")
                     val column3Value = cursor.getString(column3Index)
 
                     showToastMessage(
-                        "Descripcion: $column4Value,\n MarcaDescripcion: $column5Value, \nCantidad: $column3Value")
+                        "Code: $column4Value,\n Name: $column5Value, \ncCampaing: $column3Value")
 
                 } while (cursor.moveToNext())
             }
